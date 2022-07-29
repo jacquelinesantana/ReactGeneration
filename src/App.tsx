bdbb9093120aa237fe067aeef2935f999e6ac886
JACQUE  HERNANDES <ti.jacque@gmail.com>
2022-07-28 23:03:20 -0300

    Adicionadas as rotas para os arquivos e os componentes fixados na app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+//importar biblioteca material ui
+import { Grid, Paper } from '@material-ui/core';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import  Home  from './paginas/home/Home';
 import Navbar from './paginas/estatico/navbar/Navbar';
 import Footer from './paginas/estatico/footer/Footer';
 //<></> é um componente que chamamos de fragment no react, ele é uma forma "Simplificada" de incluir um div
 function App() {
-  return (
-    <>
-      <Navbar />
+  return( 
+  <Router> 
+     
+    <Navbar /> 
+    
+    <div style={{ minHeight: 'calc(100vh - 180px)' }}>
+    <Routes> // Antigo Switch 
+      
+      <Route path="/home" element={<Home />} /> 
+      <Route path="/cadastro" element={<Footer />} /> 
+      </Routes> 
+      </div>
 
-      <Home />
-
-      <Footer />
-    </>
-  );
+      <Footer /> 
+      
+      </Router> 
+  )
 }
 
 export default App;
